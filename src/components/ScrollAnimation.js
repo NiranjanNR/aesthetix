7b85fc18423d435deef850289bc8f5e814083d0b
NiranjanNR <kidkool8@gmail.com>
2024-04-14 01:00:44 +0530

    parallax added

--- src/components/ScrollAnimation.js
+++ src/components/ScrollAnimation.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import '../App.css'; // Import CSS for styling
+import { Parallax } from 'react-scroll-parallax';
 
 const App = () => {
   return (
-    <div className="container">
-      <main className="content">
-        <section>
-          <p>
-            <span contentEditable={true}>
-            At Aesthetix Technologies, 
-            we are re imagining communication and collaboration to design work and living spaces of the 21st century. 
-            </span>
-          </p>
-        </section>
-      </main>
-      <div className="image-section">
-        {/* Add your image here */}
-        {/* For example: */}
-        <img src="aesthetix.jpeg" alt="Image" />
+    <div>
+      <div className='h-[170px]'>
+        <Parallax scale={[1, 1.45]} translateY={['-120px', '200px']} opacity={[0.5, 5]}>
+          <div className="flex px-4 justify-center items-center">
+            <div className='text-[140px] tracking-wider text-[#4343437d] font-extrabold tracking-tight	'>ABOUT US</div>
+          </div>
+        </Parallax>
+      </div>
+      <div className="container flex justify-center">
+        <main className="content">
+          <section>
+            <p>
+              <div className='text-6xl font-bold mb-6'>Who Are We?</div>
+              <span className='w-[100px]' contentEditable={true}>
+                Vishaal is a naayinde mwon. Bro cant even pose properly, As seen in the demo photo. But lets be respectful.
+              </span>
+            </p>
+          </section>
+        </main>
+        <div className="image-section">
+          <img src="king.jpeg" alt="Image" />
+        </div>
       </div>
     </div>
   );
